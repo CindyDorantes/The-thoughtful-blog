38f50678eb023d49ab48516091f60b7e3bce2a78
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-20 15:42:51 -0600

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    belongs_to :post, counter_cache: :comments_counter
-    belongs_to :user
+  belongs_to :post, counter_cache: :comments_counter
+  belongs_to :user
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :post, counter_cache: :likes_counter
-    belongs_to :user
+  belongs_to :post, counter_cache: :likes_counter
+  belongs_to :user
 end
