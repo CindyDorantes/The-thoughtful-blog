0b404709390943c0d3c12a7aafc5e2a0c8ad1672
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-19 18:33:40 -0600

    build project schema using migration commands

---
+++ db/migrate/20230120003004_add_user_ref_to_posts.rb
@@ -0,0 +1,7 @@
+class AddUserRefToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :posts, :user, null: false, foreign_key: true
+    add_column :posts, :user, :string
+    add_index :posts, :user
+  end
+end
