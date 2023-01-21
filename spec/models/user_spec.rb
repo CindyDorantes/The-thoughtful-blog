da5a5d6b99e70ef1b4418593165a7cc7574c206c
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-20 20:11:42 -0600

    add model user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   #tests go here
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
   before { subject.save }
 
@@ -19,22 +19,22 @@ RSpec.describe User, type: :model do
     end
 
     it 'post_counter must be an integer' do
-      subject.name = 'foo'
+      subject.posts_counter = 'foo'
       expect(subject).to_not be_valid
     end
 
     it 'post_counter must be an integer greater than zero' do
-      subject.name = 0
-      expect(subject).to_not be_valid
+      subject.posts_counter = 0
+      expect(subject).to be_valid
     end
   end
 
   context 'Test User methods' do
     it 'returns last three post by user' do
-      Post.create(author: subject, title: 'Hello', text: 'This is my first post')
-      Post.create(author: subject, title: 'Hello again', text: 'This is my second post. Welcome')
-      Post.create(author: subject, title: 'Hi', text: 'Whats up folks?')
-      Post.create(author: subject, title: 'See you later', text: 'With this, I say goodbye')
+      Post.create(user: subject, title: 'Hello', text: 'This is my first post')
+      Post.create(user: subject, title: 'Hello again', text: 'This is my second post. Welcome')
+      Post.create(user: subject, title: 'Hi', text: 'Whats up folks?')
+      Post.create(user: subject, title: 'See you later', text: 'With this, I say goodbye')
       last_posts = subject.last_three_post
       expect(last_posts.length).to eq 3
     end
