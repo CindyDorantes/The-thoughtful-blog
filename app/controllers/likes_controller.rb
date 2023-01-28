fa9a57b242fef59fd89dda801e02ee4c7e306e3e
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-27 23:25:42 -0600

    allow Users to add likes to Posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,8 @@
-class CommentsController < ApplicationController
+class LikesController < ApplicationController
   def new
-    comment = Comment.new
+    like = Like.new
     respond_to do |format|
-      format.html { render :new, locals: {comment: comment} }
+      format.html { render :new, locals: {like: :like} }
     end
   end
 
@@ -10,21 +10,24 @@ class CommentsController < ApplicationController
     user = User.find(params[:user_id])
     post = Post.find(params[:post_id])
     # new object from params
-    comment = Comment.new(text: params[:new_comment][:text], post: post, user: current_user)
+    like = Like.new(post: post, user: current_user)
     # respond_to block
     respond_to do |format|
       format.html do
         # if question saves
-        if comment.save
+        if like.save
+          # success message
+          flash[:notice] = "Like added successfully"
           # redirect to index
           redirect_to user_post_path(user, post)
         else
           # error message
-          flash[:notice] = "Error: Couldn't create comment"
+          flash[:notice] = "Error: Couldn't add like"
           # render new
-          render :new, locals: { comment: comment }
+          render :new, locals: { like: like }
         end
       end
     end
   end
+  
 end
