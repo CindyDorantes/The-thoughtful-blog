f14c7358c1c1adc41b8e4b0bd3ee43abad783db4
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-27 20:18:40 -0600

    add a new Post controller action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,13 @@ class PostsController < ApplicationController
     @posts = User.find(params[:user_id]).posts
   end
 
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post }  }
+    end
+  end
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
