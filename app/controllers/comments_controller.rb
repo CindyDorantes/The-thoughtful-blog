15c553bbe52dc0a36d12736e809458cba89a71dc
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-27 23:11:51 -0600

    add the create Action for  comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,28 @@ class CommentsController < ApplicationController
       format.html { render :new, locals: {comment: comment} }
     end
   end
+
+  def create
+    user = User.find(params[:user_id])
+    post = Post.find(params[:post_id])
+    # new object from params
+    comment = Comment.new(text: params[:new_comment][:text], post: post, user: current_user)
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        # if question saves
+        if comment.save
+          # success message
+          flash[:notice] = "New comment saved successfully"
+          # redirect to index
+          redirect_to user_post_path(user, post)
+        else
+          # error message
+          flash[:notice] = "Error: Couldn't create comment"
+          # render new
+          render :new, locals: { comment: comment }
+        end
+      end
+    end
+  end
 end
