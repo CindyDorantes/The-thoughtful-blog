dc3fc3acefdca25e3d74aa456fba1db7231b2976
CindyDorantes <cindym.dorantes@gmail.com>
2023-01-27 22:25:36 -0600

    add Post controller action create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def create
+    # new object from params
+    post = Post.new(title: params[:new_post][:title], text: params[:new_post][:text], user: current_user)
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        # if question saves
+        if post.save
+          # success message
+          flash[:notice] = "New post saved successfully"
+          # redirect to index
+          redirect_to user_posts_path(current_user.id)
+        else
+          # error message
+          flash.now[:notice] = "Error: Couldn't create post"
+          # render new
+          render :new, locals: { post: post }
+        end
+      end
+    end
+  end
+  
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
