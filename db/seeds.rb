82bee2c4ba3ea6c20f68d288f5f3de0cfaaf1742
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-01 14:36:27 -0600

    add styling to user#index and user#show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=785&q=80', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1614701466929-766271250d48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwzNjc4OTAyfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', bio: 'Teacher from Poland.')
 
 first_post = Post.create(user: first_user, title: 'Hello', text: 'This is my first post')
 second_post = Post.create(user: first_user, title: 'Hello hello', text: 'This is my second post')
 third_post = Post.create(user: first_user, title: 'Hello again', text: 'This is my third post')
-fourth_post = Post.create(user: first_user, title: 'Hello x4', text: 'This is my fourth post')
+fourth_post = Post.create(user: second_user, title: 'Welcome', text: 'Great to see you here')
 
 Comment.create(post: first_post, user: second_user, text: 'Hi Tom!' )
 Comment.create(post: first_post, user: second_user, text: 'Nice post' )
