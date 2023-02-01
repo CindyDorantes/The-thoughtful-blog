f35d5e0213bc23c0acdf8b815da40e7b77b7aad3
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-01 16:05:23 -0600

    fix confirmation of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts
   has_many :comments
   has_many :likes
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_post
