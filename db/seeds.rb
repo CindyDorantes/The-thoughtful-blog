73e5607fd3d4afe4e5f411395fe9234e2a020401
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-01 22:43:20 -0600

    add an input field for the username at signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 # first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=785&q=80', bio: 'Teacher from Mexico.', email: 'tom@gmail.com')
 # second_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1614701466929-766271250d48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwzNjc4OTAyfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', bio: 'Teacher from Poland.')
 
-first_user = User.create(email: 'tom@gmail.com', password: '123abc', name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+first_user = User.create(email: 'tom@gmail.com', password: '123abc', name: 'Tom', photo: 'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=785&q=80',
   bio: 'Teacher from Mexico.')
 second_user = User.create(email: 'lilly@gmail.com', password: '123abc', name: 'Lilly', photo: 'https://images.unsplash.com/photo-1665686310934-8fab52b3821b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
   bio: 'Teacher from Poland.')
 User.create(name: 'Cindy', email: 'cindy@gmail.com', password: '123abc', bio: 'The blog Creator',
-    photo: 'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=785&q=80')
+    photo: 'https://images.unsplash.com/photo-1536164261511-3a17e671d380?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60')
 
 first_post = Post.create(user: first_user, title: 'Hello', text: 'This is my first post')
 second_post = Post.create(user: first_user, title: 'Hello hello', text: 'This is my second post')
