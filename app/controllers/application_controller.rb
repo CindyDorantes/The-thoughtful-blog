f59920722ff875693d63ae843cfa71087e390572
Shedrack Sunday <hannania002@gmail.com>
2023-02-04 14:38:32 +0000

    Refactor controller settling for API usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   before_action :authenticate_user!
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module TheThoughtfulBlog
   class Application < Rails::Application
+    config.api_only = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
